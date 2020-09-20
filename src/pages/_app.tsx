8596f0a13231186096b365eb497ab33f1c467ef1
flplima <felipe.lds@live.com>
2020-09-19 21:51:17 -0300

    styles: Update styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import "../styles/base.css";
-import "../styles/app.css";
+import "node_modules/todomvc-common/base.css";
+import "node_modules/todomvc-app-css/index.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
